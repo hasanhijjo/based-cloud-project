601350b84d03de74b6c499b56d8be278568536fc
Hasan Hijjo <131011994+hasanhijjo@users.noreply.github.com>
2025-06-17 19:21:50 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,8 @@
-import os
-import sys
+from flask import Flask
+from app.routes import app  
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from app.routes import app
+import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
