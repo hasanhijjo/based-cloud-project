1f7c7938561f4079592127042ab2c12e16f33656
Hasan Hijjo <131011994+hasanhijjo@users.noreply.github.com>
2025-06-17 19:12:44 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-import sys
 import os
+import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from app.routes import app
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  
     app.run(host="0.0.0.0", port=port)
+
